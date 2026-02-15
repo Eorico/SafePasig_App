b3318a2243e4674e29488dd8e0289f6fb808836f
Eorico Gonzales <eoricogonzales@gmail.com>
2026-02-16 00:28:16 +0800

    update the not fpimd

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,16 +1,28 @@
-import { Link, Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { Text, View, Pressable } from "react-native";
 import { notFound } from "@/app/appStyles/not.found.style";
 
 export default function NotFoundScreen() {
+  const router = useRouter();
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }}/>
+      <Stack.Screen options={{ title: "Oops!" }} />
       <View style={notFound.NF_container}>
         <Text style={notFound.NF_text}>Oops! Page not Found.</Text>
-        <Link href='/(tabs)/map' style={notFound.NF_link}>
+
+        <Pressable 
+          style={notFound.NF_link}
+          onPress={(() => {
+            if (router.canGoBack()) {
+              router.back();
+            } else {
+              router.replace("/(tabs)/map");
+            }
+          })}
+        >
           <Text>Go Back!</Text>
-        </Link>
+        </Pressable>
       </View>
     </>
   );
